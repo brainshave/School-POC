418dce0257dcd2a66d4c56ec0cf5c0c70fa6a98c
Szymon Witamborski <santamon@gmail.com>
2010-10-13 21:21:00 +0200

    Initial commit. Set up project.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject poc "0.1.0-SNAPSHOT"
+  :description "Project for Digital Image Processing course (Przetwarzanie Obrazów Cyfrowych in Polish)"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+                 ;;[org.clojure.contrib/datalog "1.3.0-alpha1"]
+		 [org.eclipse/swt-gtk-linux-x86 "3.5.2"]
+		 [little-gui-helper "0.1.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
