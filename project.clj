5a2b9358b44a1ff85ef0d31e635b421433ab6f43
Szymon Witamborski <santamon@gmail.com>
2010-10-23 20:22:16 +0200

    Merged with win32 branch with obvious change from win32 to gtk version of swt

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :jvm-opts ["-Xmx1g"]
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
-		 [org.eclipse/swt-win32-win32-x86 "3.5.2"]
+		 [org.eclipse/swt-gtk-linux-x86 "3.5.2"]
 		 [little-gui-helper "0.1.0-SNAPSHOT"]
 		 [com.miglayout/miglayout "3.7.3.1" :classifier "swt"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
